4fb18a936f540b13e708a8e6421839986307ef4a
MATEO HERRERA GUTIERREZ <mateoherrera264627@correo.itm.edu.co>
2023-04-28 21:57:30 -0500

    Controller creation

--- TicketEstadio/Examen.API/Controllers/TicketController.cs
+++ TicketEstadio/Examen.API/Controllers/TicketController.cs
@@ -1,9 +1,71 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Examen.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TicketEstadio.Shared.Entities;
 
 namespace Examen.API.Controllers
 {
+    [ApiController]
+    [Route("api/ticket")]
     public class TicketController:ControllerBase
     {
+        private readonly DataContext _context;
+
+        public TicketController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Ticket.ToListAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var ticket = await _context.Ticket
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(ticket);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Ticket ticket)
+        {
+            _context.Add(ticket);
+            await _context.SaveChangesAsync();
+            return Ok(ticket);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Ticket ticket)
+        {
+            try
+            {
+                _context.Update(ticket);
+                await _context.SaveChangesAsync();
+                return Ok(ticket);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe un ticket con el mismo id.");
+                }
+                return BadRequest(dbUpdateException.Message);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
 
 
     }
